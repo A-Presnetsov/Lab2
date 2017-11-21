b6d2302a82a3704c624feb075136ebd932a876eb
A-Presnetsov <33604502+A-Presnetsov@users.noreply.github.com>
2017-11-21 23:06:34 +0300

    Add files via upload

---
+++ lab-2/Lab2 - client/Lab2 - client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2___client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);   //Создаём UDP сокет
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("192.168.0.103"), 1918); //"Пункт назначения" пакета данных
+
+            while (true) {
+                Console.Write("Введите строку: ");
+                string str = Console.ReadLine();
+                byte[] bytes = Encoding.UTF8.GetBytes(str);
+                socket.SendTo(BitConverter.GetBytes(bytes.Length), serverEndPoint);
+                socket.SendTo(bytes, serverEndPoint);
+
+
+                byte[] buff = new byte[1024];
+                socket.Receive(buff);
+                int length = BitConverter.ToInt32(buff, 0); 
+                byte[] responseBuffer = new byte[length]; 
+                for (int i = 0; i < length;)
+                {
+                    int recvLength = socket.Receive(buff); 
+                    Buffer.BlockCopy(buff, 0, responseBuffer, i, recvLength);
+                    i += recvLength;
+                }
+                Console.WriteLine(Encoding.UTF8.GetString(buff, 0, length) + "\n");
+            }
+            socket.Close();
+        }
+    }
+}
---
+++ lab-2/Lab2 - server/Lab2 - server/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient socket = new UdpClient(1918); //Создаём сокет
+            IPEndPoint senderEndPoint = null; //Здесь будет в результате находиться адрес отправителя
+
+            Console.WriteLine("Сервер запущен");
+
+            while (true) {
+                int length = BitConverter.ToInt32(socket.Receive(ref senderEndPoint), 0); 
+                byte[] buff = new byte[length]; 
+                byte[] buff2; 
+                for (int i = 0;  i < length; i += buff2.Length)
+                {
+                    buff2 = socket.Receive(ref senderEndPoint); //Приём сообщения
+                    Buffer.BlockCopy(buff2, 0, buff, i, buff2.Length); 
+                }
+                string clientStr = Encoding.UTF8.GetString(buff);
+                Console.WriteLine("Клиент прислал строку: " + clientStr);
+                
+                byte[] bytes = Encoding.UTF8.GetBytes(clientStr.Length + " символов");
+                socket.Send(BitConverter.GetBytes(bytes.Length), 4, senderEndPoint); 
+                socket.Send(bytes, bytes.Length, senderEndPoint); 
+            }
+            Console.ReadKey();
+        }
+    }
+}
